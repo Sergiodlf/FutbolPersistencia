e403291965789bf08dcc85ac8c320c44996f609c
Sergio De la Fuente <sergio4vientos@gmail.com>
2025-10-31 10:53:10 +0100

    carpeta templates y creación carpeta persistence/scripts para los archivos .sql

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>Competición Futbol</title>
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php include 'templates/menu.php'; ?>
+    <div class="container mt-4">
